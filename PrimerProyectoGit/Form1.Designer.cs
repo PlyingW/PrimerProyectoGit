82d8d584fb913cf6d48b4af7e7ad30dddd847b9a
Marilé Cedeño <marile_147@hotmail.com>
2023-10-11 15:40:56 -0600

    Practica con git sin respuestas

--- PrimerProyectoGit/Form1.Designer.cs
+++ PrimerProyectoGit/Form1.Designer.cs
@@ -28,12 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.fondo2;
+            pictureBox1.Location = new Point(73, 181);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(707, 800);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(255, 224, 130);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(260, 464);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 41);
+            label1.TabIndex = 1;
+            label1.Text = "Instrucciones:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(255, 224, 130);
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(260, 551);
+            label2.Name = "label2";
+            label2.Size = new Size(460, 252);
+            label2.TabIndex = 2;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = Properties.Resources.boton;
+            pictureBox2.Location = new Point(1263, 258);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(214, 130);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Image = Properties.Resources.boton;
+            pictureBox3.Location = new Point(1560, 258);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(214, 130);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.Image = Properties.Resources.boton;
+            pictureBox4.Location = new Point(1263, 448);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(214, 130);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox5.Image = Properties.Resources.boton;
+            pictureBox5.Location = new Point(1560, 448);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(214, 130);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Transparent;
+            pictureBox6.Image = Properties.Resources.boton;
+            pictureBox6.Location = new Point(1263, 644);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(214, 130);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 7;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.Transparent;
+            pictureBox7.Image = Properties.Resources.boton;
+            pictureBox7.Location = new Point(1560, 644);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(214, 130);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 8;
+            pictureBox7.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(88, 194, 210);
+            label3.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(1324, 303);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 35);
+            label3.TabIndex = 9;
+            label3.Text = "Status";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.FromArgb(88, 194, 210);
+            label4.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(1624, 303);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 35);
+            label4.TabIndex = 10;
+            label4.Text = "Branch";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(88, 194, 210);
+            label5.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(1324, 493);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 35);
+            label5.TabIndex = 11;
+            label5.Text = "Pull";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.FromArgb(88, 194, 210);
+            label6.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(1324, 692);
+            label6.Name = "label6";
+            label6.Size = new Size(97, 35);
+            label6.TabIndex = 12;
+            label6.Text = "Commit";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.FromArgb(88, 194, 210);
+            label7.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(1624, 493);
+            label7.Name = "label7";
+            label7.Size = new Size(62, 35);
+            label7.TabIndex = 13;
+            label7.Text = "Push";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.FromArgb(88, 194, 210);
+            label8.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(1624, 692);
+            label8.Name = "label8";
+            label8.Size = new Size(57, 35);
+            label8.TabIndex = 14;
+            label8.Text = "Add";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.fondo1;
+            ClientSize = new Size(1902, 1033);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Práctica con git";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
